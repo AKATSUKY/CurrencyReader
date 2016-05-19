301b12c7b46a6bf577c21d1232d9462b63b31c96
Felipe Santiago <felipe.santiago@outlook.com>
2016-05-19 12:18:11 -0300

    q

---
+++ CurrencyReader.py
@@ -0,0 +1,83 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, ForeignKey
+from sqlalchemy import DateTime, Integer, Numeric, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import load_only
+import requests
+from argparse import ArgumentParser 
+from datetime import timedelta, datetime
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql:'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+session = Session(bind=engine)
+Base = declarative_base()
+
+class Currency(Base):
+    __tablename__ = "currency"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True ) 
+    code = Column(String(3), unique=True )
+    stopGetValue = Column(DateTime, nullable = True) 
+
+class CurrencyValue(Base):
+    __tablename__ = "currency_value"
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime)
+    value = Column(Numeric(10,6))
+    currencyId = Column(Integer, ForeignKey('currency.id'))
+
+Base.metadata.create_all(engine)
+
+def addNewCurrencyValue(url): 
+    print(url)
+    response = requests.get(url)
+    objs = response.json()
+    quotes = objs["quotes"]
+    currencies = session.query(Currency).filter_by(stopGetValue=None)
+    .all()
+    for currency in currencies: 
+        newCurrencyValue = CurrencyValue(timestamp = 
+        datetime.utcfromtimestamp(objs['timestamp']), 
+        value = quotes['USD' + currency.code], 
+        currencyId = currency.id)   
+        if session.query(CurrencyValue).filter_by
+        (timestamp=newCurrencyValue.timestamp, 
+        currencyId=newCurrencyValue.currencyId).first() == None:
+            session.add(newCurrencyValue)
+    session.commit()        
+
+parser = ArgumentParser()
+parser.add_argument("c", nargs='?', help="Import Currency")
+args = parser.parse_args()
+
+if args.c == 'c':
+    #Get Currencies and register in table
+    response = requests.get("http://apilayer.net/api/list?access_key=
+    &format=1")
+    objs = response.json()
+    for currency in objs["currencies"]:
+        auxCurrency = Currency(code=currency, 
+        name=objs["currencies"][currency])
+        # print auxCurrency.code
+        # print auxCurrency.name
+        if session.query(Currency).filter_by(code=auxCurrency.code)
+        .first() == None:
+            session.add(auxCurrency)
+    session.commit()
+    
+    #Get Historical Currency Values, 180 days.
+    end_date = datetime.today()
+    start_date = end_date - timedelta(days=180) 
+    delta = end_date - start_date
+    for i in range(delta.days):
+        date = (start_date + timedelta(days=i)).strftime("%Y-%m-%d")
+        url = 'http://apilayer.net/api/historical?access_key=
+        &source=USD&format=1&date='
+        + date
+        addNewCurrencyValue(url)
+else:
+    url = 'http://apilayer.net/api/live?access_key=
+    &source=USD&format=1'
+    addNewCurrencyValue(url)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CurrencyReader
-This is an small application that I have builded as portfolio
+This code is an small Python API Consumer that every day reads the currencies value from CurrencyLayer, and stores it into my AWS MariaDB Database. Picture 1
+
+It also demonstrates the results with a plot made in R, this Plot is an Line Chart with a few options for the user to filter and best view the data.
+
+The Python API Consumer also is able to receive a parameter to load historical values. It reads all the currencies in the last 180 days. Picture 2.
